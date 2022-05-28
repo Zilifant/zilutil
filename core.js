355691ac12a66c24c7ffc69601cf2555ae5f1453
Scott Silsbe <sils@impossible-bureau.com>
2022-05-28 17:59:23 -0400

    added newEnum; misc formatting chanes

--- core.js
+++ core.js
@@ -1,13 +1,13 @@
 // Core
 
-const { fp, fc, fx } = require('./functions/file-system-utility/fsUtility');
+const { fp, fc, fx } = require("./functions/file-system-utility/fsUtility");
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Get a property's value by index
 
 function gevi(obj, ind) {
   return obj[Object.keys(obj)[ind]];
-};
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Get a property's key by...
@@ -15,12 +15,12 @@ function gevi(obj, ind) {
 // ...index
 function geki(obj, ind) {
   return Object.keys(obj)[ind];
-};
+}
 
 // ...value
 function gekv(obj, val) {
   return Object.keys(obj)[Object.values(obj).indexOf(val)];
-};
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Gevik methods mixin
@@ -28,108 +28,120 @@ function gekv(obj, val) {
 function mixGevik(obj) {
   obj = {
     ...obj,
-    ...gevik()
+    ...gevik(),
   };
   Object.defineProperties(obj, {
     gevi: { enumerable: true },
     geki: { enumerable: true },
-    gekv: { enumerable: true }
+    gekv: { enumerable: true },
   });
   return obj;
-};
+}
 
 function gevik() {
   let gevik = {};
   Object.defineProperties(gevik, {
     gevi: {
-      value: function(ind) {
+      value: function (ind) {
         return this[Object.keys(this)[ind]];
       },
-      enumerable: true
+      enumerable: true,
     },
     geki: {
-      value: function(ind) {
+      value: function (ind) {
         return Object.keys(this)[ind];
       },
-      enumerable: true
+      enumerable: true,
     },
     gekv: {
-      value: function(val) {
+      value: function (val) {
         return Object.keys(this)[Object.values(this).indexOf(val)];
       },
-      enumerable: true
-    }
+      enumerable: true,
+    },
   });
   return gevik;
-};
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Add new private property (e.g. with getter/setter)
 
 function pvp(obj, key) {
   Object.defineProperty(obj, key, {
-    get: function() { return this[`_${key}`] },
-    set: function(newVal) { this[`_${key}`] = newVal },
-    enumerable: true
+    get: function () {
+      return this[`_${key}`];
+    },
+    set: function (newVal) {
+      this[`_${key}`] = newVal;
+    },
+    enumerable: true,
   });
-};
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Random integer within a given range (inclusive)
 
-function rInt(min,max) {
+function rInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
-};
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Shuffle an array
 // Credit to: https://bost.ocks.org/mike/shuffle/
 
 function shuffle(array) {
-  let m = array.length, t, i;
+  let m = array.length,
+    t,
+    i;
   while (m) {
     i = Math.floor(Math.random() * m--);
     t = array[m];
     array[m] = array[i];
     array[i] = t;
-  };
+  }
   return array;
-};
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Return new object with specified properties removed
 
 function omit(obj, keys) {
   const newObj = Object.assign({}, obj);
-  keys.forEach(key => {
+  keys.forEach((key) => {
     if (newObj[key]) delete newObj[key];
   });
   return newObj;
-};
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Return random element from array
 
 function sample(arr) {
-  return arr[Math.floor(Math.random()*arr.length)];
-};
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Coin flip, fair or weighted
 // pt = percent true (heads); no value = 50/50
 
+/**
+ * Flip a coin, fair or weighted.
+ * @param {number} [n=50] - A number between 1 and 100, which will be the
+ * percent chance the result is `true`.
+ */
+
 function coinFlip(pt) {
-  return pt ? Math.random() < (pt / 100) : Math.random() < 0.5;
-};
+  return pt ? Math.random() < pt / 100 : Math.random() < 0.5;
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Capitalize first letter of a string
 
 function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
-};
+}
 
 // Capitalize first letter of each word in a string
 
@@ -138,71 +150,123 @@ function capitalizeAll(str) {
     /\b([a-zÁ-ú])/g,
     (w) => w.charAt(0).toUpperCase() + w.slice(1)
   );
-};
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Remove duplicate elements from an array
 
 function rmDupes(array) {
-  return array.reduce((unique, item) => unique.includes(item) ? unique : [...unique, item], []);
-};
+  return array.reduce(
+    (unique, item) => (unique.includes(item) ? unique : [...unique, item]),
+    []
+  );
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Select a random name from a curated list
 
 function curatedName(list) {
   const names = list || [
-    'nemo','cassandra','olivia','ragnar','brand','hera','violet','dagney','baldur','sigurd','rhialto','corwin','bethany','zane','athena','dane','fili','kili','gloin','chang','sandor','gerrard','freya','zanzel','madeline','arronax'
+    "nemo",
+    "cassandra",
+    "olivia",
+    "ragnar",
+    "brand",
+    "hera",
+    "violet",
+    "dagney",
+    "baldur",
+    "sigurd",
+    "rhialto",
+    "corwin",
+    "bethany",
+    "zane",
+    "athena",
+    "dane",
+    "fili",
+    "kili",
+    "gloin",
+    "chang",
+    "sandor",
+    "gerrard",
+    "freya",
+    "zanzel",
+    "madeline",
+    "arronax",
   ];
-  const name = names[Math.floor(Math.random()*names.length)];
-  return name[0].toUpperCase()+name.slice(1);
-};
+  const name = names[Math.floor(Math.random() * names.length)];
+  return name[0].toUpperCase() + name.slice(1);
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Generate a unique name
 
 function uniqName(minLen, maxLen) {
-  const
-  cons = {
-    c: ['w','r','t','p','s','d','f','g','h','j',
-        'l','c','v','b','n','m'],
-    u: ['ch','gh','th','sp','st','sh','sm','ph'],
-    r: ['tt','ss','ll','bl','fl','gl','sl','pl',
-        'x','z']
-  },
-  vows = {
-    c: ['a','e','i','o','u'],
-    u: ['ai','ao','au',
-        'ea','ei','eo',
-        'ia','ie','io',
-        'oa','oe','oi','ou',
-        'ua','ui'],
-    r: ['ae','ue','uo','oo','ee','eu','iu']
-  },
-  bad_words = [
-    '\u0073\u0068\u0069\u0074', // s**t
-    '\u0070\u0069\u0073\u0073', // p**s
-    '\u0063\u0075\u006e\u0074', // c**t
-    '\u0061\u0073\u0073',       // a**
-    '\u0070\u0075\u0073\u0073', // p**s
-    '\u0066\u0075\u0063\u006b', // f**k
-    '\u0072\u0061\u0070\u0065', // r**e
-  ],
-  bad_starts = ['oo','ee','tt','ss','ll','oa','ua'],
-  bad_ends = ['sp','gl','fl','pl','bl','ph','j'],
-  short = Math.random() < 0.5,
-  len = short ? 1 : 2,
-  min_len = minLen || 4,
-  max_len = maxLen || 7,
-  letters = [],
-  fir_let_con = Math.random() < 0.5,
-  sample = arr => arr[Math.floor(Math.random()*arr.length)];
+  const cons = {
+      c: [
+        "w",
+        "r",
+        "t",
+        "p",
+        "s",
+        "d",
+        "f",
+        "g",
+        "h",
+        "j",
+        "l",
+        "c",
+        "v",
+        "b",
+        "n",
+        "m",
+      ],
+      u: ["ch", "gh", "th", "sp", "st", "sh", "sm", "ph"],
+      r: ["tt", "ss", "ll", "bl", "fl", "gl", "sl", "pl", "x", "z"],
+    },
+    vows = {
+      c: ["a", "e", "i", "o", "u"],
+      u: [
+        "ai",
+        "ao",
+        "au",
+        "ea",
+        "ei",
+        "eo",
+        "ia",
+        "ie",
+        "io",
+        "oa",
+        "oe",
+        "oi",
+        "ou",
+        "ua",
+        "ui",
+      ],
+      r: ["ae", "ue", "uo", "oo", "ee", "eu", "iu"],
+    },
+    bad_words = [
+      "\u0073\u0068\u0069\u0074", // s**t
+      "\u0070\u0069\u0073\u0073", // p**s
+      "\u0063\u0075\u006e\u0074", // c**t
+      "\u0061\u0073\u0073", // a**
+      "\u0070\u0075\u0073\u0073", // p**s
+      "\u0066\u0075\u0063\u006b", // f**k
+      "\u0072\u0061\u0070\u0065", // r**e
+    ],
+    bad_starts = ["oo", "ee", "tt", "ss", "ll", "oa", "ua"],
+    bad_ends = ["sp", "gl", "fl", "pl", "bl", "ph", "j"],
+    short = Math.random() < 0.5,
+    len = short ? 1 : 2,
+    min_len = minLen || 4,
+    max_len = maxLen || 7,
+    letters = [],
+    fir_let_con = Math.random() < 0.5,
+    sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
   const letter = (cv) => {
     const n = Math.random();
-    return n >= .4 ? sample(cv.c)
-         : n >= .1 ? sample(cv.u)
-         : sample(cv.r)
+    return n >= 0.4 ? sample(cv.c) : n >= 0.1 ? sample(cv.u) : sample(cv.r);
   };
 
   for (let i = 0; i < len; i++) {
@@ -212,21 +276,21 @@ function uniqName(minLen, maxLen) {
     } else {
       letters.push(letter(vows));
       letters.push(letter(cons));
-    };
-  };
+    }
+  }
 
-  const name = letters.join('');
+  const name = letters.join("");
   const bads = [
-    bad_starts.includes(name.slice(0,2)),
+    bad_starts.includes(name.slice(0, 2)),
     bad_ends.includes(name.slice(-2)),
     bad_words.includes(name),
     name.length < min_len,
-    name.length > max_len
+    name.length > max_len,
   ];
-  const bad = bads.some(el => el)
+  const bad = bads.some((el) => el);
 
   return bad ? uniqName() : name;
-};
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Memoize; save result of a fn to use/reuse later
@@ -242,7 +306,7 @@ function memoize(fn) {
     cache.set(key, result);
     return result;
   };
-};
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Repeat a function
@@ -250,8 +314,8 @@ function memoize(fn) {
 function repeat(n, fn) {
   for (let i = 0; i < n; i++) {
     return fn();
-  };
-};
+  }
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Run Array.forEach in reverse order
@@ -259,22 +323,33 @@ function repeat(n, fn) {
 function forEachReverse(arr, fn) {
   for (let i = arr.length - 1; i >= 0; i--) {
     return fn();
-  };
-};
+  }
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Return array of values that appear in both arrays
 
 function inter(arr1, arr2) {
-  return arr1.filter(x => arr2.includes(x));
-};
+  return arr1.filter((x) => arr2.includes(x));
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Return array of values that appear in arr1 but not arr2
 
 function diff(arr1, arr2) {
-  return arr1.filter(x => !arr2.includes(x));
-};
+  return arr1.filter((x) => !arr2.includes(x));
+}
+
+// ~~~~~~~~~~~~~~~~~~~~
+// Return an 'enum', a frozen object
+
+function newEnum(vals) {
+  const enumObject = {};
+  for (const val of vals) {
+    enumObject[val] = val;
+  }
+  return Object.freeze(enumObject);
+}
 
 // ~~~~~~~~~~~~~~~~~~~~
 // Check if string is a valid URL
@@ -316,6 +391,7 @@ module.exports = {
   forEachReverse,
   inter,
   diff,
+  newEnum,
   urlRegex,
   emailRegex,
 };
